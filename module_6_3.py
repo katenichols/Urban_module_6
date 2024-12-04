19f5a7cd7278179a6ad8e8d708e7aa4c3f5bc4b4
KateNichols <kat-nik79@ya.ru>
2024-12-04 22:00:42 +0500

    Попытка №1 ДЗ модуля 6.3.

--- module_6_3.py
+++ module_6_3.py
@@ -6,9 +6,9 @@ class Animal:
     live = True
     sound = None
 
-    def __init__(self):
-        _cords = [0, 0, 0]
-        speed = ...
+    def __init__(self, speed):
+        self._cords = [0, 0, 0]
+        self.speed = speed
 
     def move(self, dx, dy, dz):
         self._cords[0] = dx * self.speed
@@ -18,10 +18,8 @@ class Animal:
         else:
             self._cords[2] = dz * self.speed
 
-
     def get_cords(self):
         print(f"X: {self._cords[0]}, Y: {self._cords[1]}, Z: {self._cords[2]}")
-
 
     def attack(self):
         if self._DEGREE_OF_DANGER < 5:
@@ -37,9 +35,9 @@ class Bird(Animal):
     beak = True
 
     def lay_eggs(self):
-        count_of_eggs = random.randint(1, 5)
+        count_of_eggs = random.randint(1, 4)
         if count_of_eggs == 1:
-            print(f"Here is {count_of_eggs} eggs for you")
+            print(f"Here is {count_of_eggs} egg for you")
         else:
             print(f"Here are {count_of_eggs} eggs for you")
 
@@ -48,17 +46,33 @@ class AquaticAnimal(Animal):
     _DEGREE_OF_DANGER = 3
 
     def dive_in(self, dz):
-        super()._cords[2] = abs(super()._cords[2]) * (self.speed / 2)
+        if self._cords[2] > dz:
+            self._cords[2] = dz
+        else:
+            self._cords[2] = abs(self._cords[2] - dz)
 
 
 class PoisonousAnimal(Animal):
     _DEGREE_OF_DANGER = 8
 
 
-class Duckbill(Bird, AquaticAnimal, PoisonousAnimal):
+class Duckbill(Bird, PoisonousAnimal, AquaticAnimal):
     sound = "Click-click-click"
+
+    def __init__(self, speed):
+        super().__init__(speed)
 
 
 db = Duckbill(10)
 print(db.live)
 print(db.beak)
+
+db.speak()
+db.attack()
+
+db.move(1, 2, 3)
+db.get_cords()
+db.dive_in(6)
+db.get_cords()
+
+db.lay_eggs()
